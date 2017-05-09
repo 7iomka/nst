4ab4c973609794da7db22134d0241a780d413933
7iomka <7iomka@gmail.com>
2017-05-09 15:48:17 +0300

    удалено лишнее в ./inc

--- src/inc/mysql.php
+++
@@ -1,7 +0,0 @@
-<?php
- include_once "db_conf.php";
-
- $link = mysql_connect ($database_host, $database_username, $database_password);
- mysql_select_db ($database_name, $link);
- mysql_query("SET NAMES UTF8");
-?>
--- src/inc/pdo.php
+++
@@ -1,7 +0,0 @@
-<?php
- include_once "db_conf.php";
-
- $DBH = new PDO("mysql:host=" . $database_host . ";dbname=".$database_name, $database_username, $database_password);  
- $DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );  
- $DBH->prepare('SET NAMES UTF8')->execute();
-?>
